b59210324c393060ed5f9f5d3991cf5e8b2bcadd
Nate Clark <nate@nateclark.com>
2023-01-22 14:10:23 -0500

    automate build and release of manifests

---
+++ scripts/update-espwebtools-manifests.rb
@@ -0,0 +1,28 @@
+require 'uri'
+require 'json'
+
+manifest = {
+  name: ENV['MANIFEST_NAME'],
+  version: ENV['MANIFEST_VERSION'],
+  builds: []
+}
+
+if ENV['ESP8266_IMAGE_URI'].present?
+  manifest.builds << {
+    chipFamily: 'ESP8266',
+    parts: [
+      { path: ENV['ESP8266_IMAGE_URI'], offset: 0 }
+    ]
+  }
+end
+
+if ENV['ESP32_IMAGE_URI'].present?
+  manifest.builds << {
+    chipFamily: 'ESP32',
+    parts: [
+      { path: ENV['ESP32_IMAGE_URI'], offset: 0 }
+    ]
+  }
+end
+
+File.write("manifests/#{ENV['MANIFEST_FNAME']}", JSON.dump(manifest))
