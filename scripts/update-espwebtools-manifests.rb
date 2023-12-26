7258576475bbe1a55c0bbbb711f5a52b1556b23d
Nate Clark <nate@nateclark.com>
2023-12-26 15:39:17 -0500

    Cleanup and re-organize config files by model and version

--- scripts/update-espwebtools-manifests.rb
+++ scripts/update-espwebtools-manifests.rb
@@ -24,6 +24,15 @@ if ENV['ESP32_IMAGE_URI']
     ]
   }
 end
+
+if ENV['ESP32_S3_IMAGE_URI']
+  manifest[:builds] << {
+    chipFamily: 'ESP32-S3',
+    parts: [
+      { path: ENV['ESP32_S3_IMAGE_URI'], offset: 0 }
+    ]
+  }
+end
 json = JSON.dump(manifest)
 puts json
 File.write("install-konnected/manifests/#{ENV['MANIFEST_FNAME']}", json)
