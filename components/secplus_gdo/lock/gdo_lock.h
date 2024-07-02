140444616ff6800f055aed95b23179941d19ee7d
Ryan Powell <powell.rg@gmail.com>
2024-07-01 21:24:53 -0600

    Add logs for door/light/lock state changes.

--- components/secplus_gdo/lock/gdo_lock.h
+++ components/secplus_gdo/lock/gdo_lock.h
@@ -27,20 +27,15 @@ namespace secplus_gdo {
     class GDOLock : public lock::Lock, public Component {
         public:
         void set_state(gdo_lock_state_t state) {
-            if (state == GDO_LOCK_STATE_LOCKED && this->state == lock::LockState::LOCK_STATE_LOCKED) {
-                return;
-            }
-            if (state == GDO_LOCK_STATE_UNLOCKED && this->state == lock::LockState::LOCK_STATE_UNLOCKED) {
+            if (state == this->lock_state_) {
                 return;
             }
 
-            auto call = this->make_call();
-            if (state == GDO_LOCK_STATE_LOCKED) {
-                call.set_state(lock::LockState::LOCK_STATE_LOCKED);
-            } else if (state == GDO_LOCK_STATE_UNLOCKED) {
-                call.set_state(lock::LockState::LOCK_STATE_UNLOCKED);
-            }
-            this->control(call);
+            this->lock_state_ = state;
+            ESP_LOGI(TAG, "Lock state: %s", gdo_lock_state_to_string(state));
+            this->publish_state(state == GDO_LOCK_STATE_LOCKED ?
+                                         lock::LockState::LOCK_STATE_LOCKED :
+                                         lock::LockState::LOCK_STATE_UNLOCKED);
         }
 
         void control(const lock::LockCall& call) override {
@@ -51,9 +46,11 @@ namespace secplus_gdo {
             } else if (state == lock::LockState::LOCK_STATE_UNLOCKED) {
                 gdo_unlock();
             }
-
-            this->publish_state(state);
         }
+
+        private:
+        gdo_lock_state_t lock_state_{GDO_LOCK_STATE_MAX};
+        static constexpr const char* TAG = "GDOLock";
     };
 
 } // namespace secplus_gdo
